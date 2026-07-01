6aba6e1483147a7d3673bdbc48355d3bb06d8660
Karoline <213629908+raftergoose@users.noreply.github.com>
2026-07-01 19:22:24 +0000

    Use absolute paths in entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 DB_PATH="${LUNAR_DB_PATH:-db/game.db}"
 
 mkdir -p $(dirname $DB_PATH)
-goose -dir migrations sqlite3 "$DB_PATH" up
+goose -dir /opt/lunar-tear/migrations sqlite3 "$DB_PATH" up
 
 AUTH_FLAG=""
 if [ -n "${LUNAR_AUTH_URL}" ]; then
@@ -16,7 +16,7 @@ if [ -n "${LUNAR_ADMIN_LISTEN}" ]; then
   ADMIN_FLAG="--admin-listen ${LUNAR_ADMIN_LISTEN}"
 fi
 
-exec ./lunar-tear \
+exec /opt/lunar-tear/lunar-tear \
   --listen "${LUNAR_LISTEN:-0.0.0.0:443}" \
   --public-addr "${LUNAR_PUBLIC_ADDR}" \
   --octo-url "${LUNAR_OCTO_URL}" \
